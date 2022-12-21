91dd5db5cb849cda874be9e5d07d7f23ebd7c8d9
swathikrishnaus <swathikrishnaus2015@gmail.com>
2022-12-21 15:21:30 +0530

    added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,25 @@
 $(".dropdown-btn").click(function () {
     $(this).find("i").toggleClass("rotate-180");
 })
+
+
+
+
+
+
+
+// --------------------Profile password show start----------------------------
+
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  // --------------------Profile password end----------------------------
+
+
+
